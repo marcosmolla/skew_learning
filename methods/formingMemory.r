8a8c866d7e392b7a012827eecf021870064344f5
Marco Smolla <ms2070@me.com>
2019-05-22 09:58:06 +0200

    Further cleaning up of the code

--- methods/formingMemory.r
+++ methods/formingMemory.r
@@ -8,21 +8,11 @@ setPayoffs <- function(setup, patDF){
 }
 
 choosePatches <- function(setup, patPayoff, observable, ID2, patDF){
-	# Standard or Preferential Learning?
-	# If setup$preferentialLearning does not exist Standard is used by default
-	ifelse(test=!is.null(setup$preferentialLearning), yes=prefL<-setup$preferentialLearning, no=FALSE)
 	observed <- unlist(lapply(observable[ID2], function(what){
 		if(!prefL){
-			## STANDARD LEARNING ===
-			## the follwoing line is the standard sampling of any patch informatin that is not 0
 			mySample(what[what!=0], size = 1, replace = T)
 		} else {
-			## PREFERENTIAL LEARNING ===
-			## with the following line I alter the standard selection process so that individual always choose the individual that provides patch information with the higher payoff
-			# First, extract payoffs (+1, because first patch is patch 0, which doesn't count)
-			# pay<-patPayoff[what] # this doesn't work when ID's are mixed in the patDF, I need to find the right row in the data.frame that corresponds to the patch ID the learner wants to learn about
 			pay <- patPayoff[match(what,patDF[,'id'])]
-			# Second, if there are any patches known by neighbours and they weren't previously learning, choose the the patch with the highest payoff, if several are equal in payoff pick one at random
 			ifelse(test=length( what[what!=0] )!=0, yes=mySample(what[which(pay==max(pay,na.rm=T))],1), no=NA)
 		}
 	}))
@@ -37,10 +27,9 @@ choosePatches <- function(setup, patPayoff, observable, ID2, patDF){
 
 
 setGeneric('formingMemory', function(setup, patDF, indDF, indDF_repertoir, indDF_repertoir_source, observable, mod, net) standardGeneric('formingMemory'))
-### Version for base and base_network
 setMethod(f = 'formingMemory', signature = c(setup='data.frame', patDF='matrix', indDF='matrix', indDF_repertoir='ANY', indDF_repertoir_source='ANY', observable='ANY', mod='character', net='ANY'), definition = function(setup, patDF, indDF, indDF_repertoir, indDF_repertoir_source, observable, mod, net){
 
-	### Consolidate repertoir due to INNOVATION
+	### Consolidate repertoir due to Individual Learning
 	patPayoff <- setPayoffs(setup, patDF)
   learn <- indDF[,'learn']
   ID <- which(learn == 1 & !is.na(learn)) #'innovate'
@@ -54,24 +43,23 @@ setMethod(f = 'formingMemory', signature = c(setup='data.frame', patDF='matrix',
 	  }
   }
 
-  ### Consolidate repertoir due to IMITATION
+  ### Consolidate repertoir due to Social Learning
   ID2 <- indDF[,'id'][ learn == 0 & !is.na(learn)] #'imitate'
 
-	# for standard model
-	  if(is.matrix(observable)) token<-sum(observable[,2], na.rm = T) > 0 else token<-observable[2] > 0 #either matrix or vector
-	  if(length(ID2) > 0 && token){ # if someone observes and something is observable
-		  	observed <- unlist(lapply(ID2, function(who){
-					if(!is.matrix(observable)){ #there is only one line of results and it is transformed to a vector
-						ifelse(who==observable[1], return(0), return(observable[2]))
-					} else { #there is more than one line, a matrix is evaluated
-			  		x <- observable[matrix(observable,ncol=2)[,1]!=who,2] # with this piece I avoid picking information from the pool that the observer added in the last round (as a demonstrator)
-			  		mySample(n = x, size = 1, replace = T)
-					}
-		  	}))
-		  	observed[is.na(observed)] <- 0
-	  }
+  if(is.matrix(observable)) token<-sum(observable[,2], na.rm = T) > 0 else token<-observable[2] > 0 #either matrix or vector
+  if(length(ID2) > 0 && token){ # if someone observes and something is observable
+	  	observed <- unlist(lapply(ID2, function(who){
+				if(!is.matrix(observable)){ #there is only one line of results and it is transformed to a vector
+					ifelse(who==observable[1], return(0), return(observable[2]))
+				} else { #there is more than one line, a matrix is evaluated
+		  		x <- observable[matrix(observable,ncol=2)[,1]!=who,2] # with this piece I avoid picking information from the pool that the observer added in the last round (as a demonstrator)
+		  		mySample(n = x, size = 1, replace = T)
+				}
+	  	}))
+	  	observed[is.na(observed)] <- 0
+  }
 
-if(exists('observed')){ #for network and standard model
+if(exists('observed')){ 
 	  j <- cbind(ID2, (observed+1))
 		indDF_repertoir[j] <- patPayoff[ (observed+1) ] # now individuals learn which payoff the patch has, i.e. if there are already individuals they learn competition payoff
 	}
--- methods/run.r
+++ methods/run.r
@@ -5,15 +5,16 @@ run <- function(setup, ceed=NULL){
   set.seed(ceed)
   setup$seed <- ceed
   
-  ### Defining the data objects
-  patDF <- as.matrix(data.frame(id=0:(setup$nPat), payoff=0, agentsPresent=NA, competitionPayOff=NA))#;patDF #formerly known as world@patch ### Patches in the world
-  indDF <- as.matrix(data.frame(id=1:setup$nInd, nRound=1, nExploit=0, innovateProp=NA, atPatch=0, learn=NA, fitness=0, yield=0, yield2=0, S=0, fitness_variance=0, m1=NA,f1=NA))#;indDF ### Single data to be stored for every living individual (row-wise)
+  ### Patches in the world
+  patDF <- as.matrix(data.frame(id=0:(setup$nPat), payoff=0, agentsPresent=NA, competitionPayOff=NA))
+  ### Single data to be stored for every living individual (row-wise)
+  indDF <- as.matrix(data.frame(id=1:setup$nInd, nRound=1, nExploit=0, innovateProp=NA, atPatch=0, learn=NA, fitness=0, yield=0, yield2=0, S=0, fitness_variance=0, m1=NA,f1=NA)) 
   
   ### Multiple data stored for every individual (column-wise)
-  indDF_repertoir <- matrix(NA,nrow=setup$nInd, ncol=(setup$nPat+1)) #nrow is max 100 because all individuals die
-  indDF_collectedPayoff <- matrix(NA, ncol=setup$maxAge, nrow=setup$nInd)#columns are rounds, rows are indviduals
+  indDF_repertoir <- matrix(NA,nrow=setup$nInd, ncol=(setup$nPat+1)) # nrow is max 100 because all individuals die
+  indDF_collectedPayoff <- matrix(NA, ncol=setup$maxAge, nrow=setup$nInd) # columns are rounds, rows are indviduals
   
-  patDF <- changeWorld(patDF=patDF, pat=nrow(patDF), create = T, payoffDis = as.character(setup$payoffDis), mod = '', setup=setup)# ;patDF
+  patDF <- changeWorld(patDF=patDF, pat=nrow(patDF), create = T, payoffDis = as.character(setup$payoffDis), mod = '', setup=setup)
   # Set males and females; 1=female, 0=male
   if( round(setup$SexStratProp*setup$nInd)>0 ) indDF[ 1:round(setup$SexStratProp*setup$nInd) ,'S'] <- 1
   # Select columns to set genetic predisposition for individual learning, for male parent 1/2, and female parent 1/2 gene
@@ -35,20 +36,21 @@ run <- function(setup, ceed=NULL){
   
   while(!allSame){
     time <- time+1
-    patDF           <- changeWorld(patDF=patDF, pat=nrow(patDF), create = F, payoffDis = as.character(setup$payoffDis), mod='', setup=setup) #;patDF
-    observable 			<- observation(indDF) ;observable
-    indDF 					<- whichAction(indDF = indDF, nInd = setup$nInd, indDF_repertoir = indDF_repertoir, patDF=patDF, setup=setup) #;indDF
-    indDF 					<- whichLearning(nInd = setup$nInd, indDF = indDF) ;indDF
-    indDF_repertoir <- formingMemory(setup = setup, patDF = patDF, indDF = indDF, indDF_repertoir = indDF_repertoir, observable = observable, mod='') #;indDF_repertoir
-    patDF           <- individualsAtPatch(patDF = patDF, indDF = indDF, setup=setup) #;patDF
-    indDF           <- whichPayoff(setup = setup, patDF = patDF, indDF = indDF, mod = '') #;indDF
-    indDF_repertoir <- updateMemory(indDF_repertoir = indDF_repertoir, indDF = indDF, mod=setup$mod) #;indDF_repertoir
+    patDF           <- changeWorld(patDF=patDF, pat=nrow(patDF), create = F, payoffDis = as.character(setup$payoffDis), mod='', setup=setup)
+    observable 			<- observation(indDF)
+    indDF 					<- whichAction(indDF = indDF, nInd = setup$nInd, indDF_repertoir = indDF_repertoir, patDF=patDF, setup=setup)
+    indDF 					<- whichLearning(nInd = setup$nInd, indDF = indDF)
+    indDF_repertoir <- formingMemory(setup = setup, patDF = patDF, indDF = indDF, indDF_repertoir = indDF_repertoir, observable = observable, mod='')
+    patDF           <- individualsAtPatch(patDF = patDF, indDF = indDF, setup=setup)
+    indDF           <- whichPayoff(setup = setup, patDF = patDF, indDF = indDF, mod = '')
+    indDF_repertoir <- updateMemory(indDF_repertoir = indDF_repertoir, indDF = indDF, mod=setup$mod)
     alive <- which(indDF[,"nRound"]>0)
     learn <- which( !is.na(indDF[,'learn']) & (indDF[,"nRound"] > 0) )
     indDF_collectedPayoff[ cbind(alive,indDF[alive,"nRound"]) ] <- indDF[alive,"yield"]
-    indDF_collectedPayoff[ cbind(learn,indDF[learn,"nRound"]) ] <- NA #;indDF_collectedPayoff[,1:10] # use NAs to indicate learning turns; otherwise they overly increase the variance
-    indDF           <- updatePayoffs(indDF = indDF, indDF_collectedPayoff=indDF_collectedPayoff, setup = setup) #;indDF
-    indDF           <- ageAndDie(setup = setup, indDF = indDF, mod='') ;indDF
+    indDF_collectedPayoff[ cbind(learn,indDF[learn,"nRound"]) ] <- NA # use NAs to indicate learning turns; otherwise they overly increase the variance
+    indDF           <- updatePayoffs(indDF = indDF, indDF_collectedPayoff=indDF_collectedPayoff, setup = setup)
+    indDF           <- ageAndDie(setup = setup, indDF = indDF, mod='')
+    
     if(sum(indDF[,'nRound']<0) > 0){
       for(h in which(indDF[,'nRound']<0)){
         indDF_repertoir[h,1:(setup$nPat+1)] <- NA
